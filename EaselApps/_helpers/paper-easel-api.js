01badfc3d27b93dbb89b25c4cf57486dbe5ed434
firetunes <curling247@gmail.com>
2019-05-24 12:37:01 -0500

    Adding Helpers

---
+++ EaselApps/_helpers/paper-easel-api.js
@@ -0,0 +1,111 @@
+
+    function GeneratePaperPaths(Volumes) {
+      
+      Volumes.forEach(function(Volume) {
+        var path;
+        var shape = Volume.shape;
+        
+        switch (shape.type) {
+          case 'rectangle':
+            path = new Path.Rectangle(new Point(shape.center.x - shape.width/2, shape.center.y - shape.height/2), new Size(shape.width, shape.height));
+            break;
+          case 'ellipse':
+            path = new Path.Ellipse(new Rectangle(new Point(shape.center.x - shape.width/2, shape.center.y - shape.height/2), new Size(shape.width, shape.height)));
+            break;
+          case 'polygon':
+          case 'path':
+          case 'polyline':
+            var cp = new CompoundPath();
+            pointarrs = shape.points;
+            if(shape.type == "polygon") {
+              pointarrs = [shape.points];
+            }
+            pointarrs.forEach(function(pointarr) {
+              var pathsegs = [];
+              pointarr.forEach(function(pt){
+                var point = new Point(pt.x, pt.y);
+                var handleIn = (pt.lh) ? (new Point(pt.lh.x, pt.lh.y)) : null;
+                var handleOut = (pt.rh) ? (new Point(pt.rh.x, pt.rh.y)) : null;
+                pathsegs.push(new Segment(point, handleIn, handleOut));
+              });
+              path = new Path({segments: pathsegs});
+              path.closed = (shape.points[0][0] == shape.points[0][shape.points[0].length - 1]);
+              
+              cp.addChild(path);
+            });
+            cp.fullySelected = true;
+            path = cp.unite();
+            
+            break;
+          default:
+            failure("Shape " + shape.type + " cannot be combined.");
+            return false;
+        }
+        
+        var bounds = path.bounds;
+        var scalex = shape.width / bounds.width;
+        var scaley = shape.height / bounds.height;
+        path.scale(scalex, scaley);
+        path.position = new Point(shape.center.x, shape.center.y);
+        
+        if(shape.flipping.horizontal) {
+          path.scale(-1, 1);
+        }
+        if(shape.flipping.vertical) {
+          path.scale(1, -1);
+        }
+        
+        path.rotate(shape.rotation * 180 / Math.PI);
+        Volume.PaperPath = path;
+        //paths.push(path);
+      });
+    }
+    
+    function PaperToEasel(Volumes) {
+      Volumes.forEach(function(Volume) {
+        var PtArrs = [];
+        var PtArr = [];
+        var SegArr = [];
+      
+        var nPath = Volume.PaperPath;
+        
+        switch(nPath.className) {
+          case 'Path':
+            PtArr = [];
+            SegArr = nPath.segments;
+            SegArr.forEach(function(Seg) {
+              var point = {
+                x: Seg.point.x,
+                y: Seg.point.y,
+                lh: {x: Seg.handleIn.x, y: Seg.handleIn.y},
+                rh: {x: Seg.handleOut.x, y: Seg.handleOut.y}
+              };
+              PtArr.push(point);
+            });
+            PtArr.push(PtArr[0]);
+            PtArrs.push(PtArr);
+            break;
+          case 'CompoundPath':
+            nPath.children.forEach(function(child){
+              PtArr = [];
+              SegArr = child.segments;
+              SegArr.forEach(function(Seg) {
+                var point = {
+                  x: Seg.point.x,
+                  y: Seg.point.y,
+                  lh: {x: Seg.handleIn.x, y: Seg.handleIn.y},
+                  rh: {x: Seg.handleOut.x, y: Seg.handleOut.y}
+                };
+                PtArr.push(point);
+              });
+              PtArr.push(PtArr[0]);
+              PtArrs.push(PtArr);
+            });
+            break;
+          default:
+        }
+        
+        var newVol = EASEL.pathUtils.fromPointArrays(PtArrs);
+        Volume.shape = newVol.shape;
+      });
+    }
